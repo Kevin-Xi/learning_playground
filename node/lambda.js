b2d3c7599c90c261c581b4fe37dac0f7e15406c7
Kevin Xi <kevin.xgr@gmail.com>
2016-11-26 18:59:54 +0800

    Make the 1st version lambda easier.

--- node/lambda.js
+++ node/lambda.js
@@ -1,13 +1,9 @@
-const C = c => () => c;
-const I = () => i => i;
-
 // the core def
 const T = x => y => x;
 const F = x => y => y;
 
 const NOT = b => b(F)(T);
-// const AND = b1 => b2 => b1(b2)(F);
-const AND = b1 => b1(b2 => b2(T)(F))(F);
+const AND = b1 => b2 => b1(b2)(F);
 const OR = b1 => b2 => NOT(AND(NOT(b1))(NOT(b2)));
 
 
