42137824f94194735ff62220fac34669ed345b80
Kevin Xi <kevin.xgr@gmail.com>
2017-04-29 23:04:33 +0800

    Complete 1st tutorial.

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
 use std::io;    // Its `import` and namespace
+use std::cmp::Ordering;
+use rand::Rng;  // use `cargo doc --open` to check docs of whole project
 
 fn main() {
     println!("Guess the number!");  // `!` means macro
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();  // default immutable
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();  // default immutable
 
-    println!("You guessed: {}", guess); // String template
+        io::stdin().read_line(&mut guess)   // return a value of type io:Result
+            .expect("Failed to read line"); // Result often has a method expect()
+                                            // if Err will cause crash
+                                            // if Ok will take the value and return
+
+        // guess is shadowed
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess); // String template
+
+        match guess.cmp(&secret_number) {   // pattern matching work along with type Ordering, which is an Enum
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
