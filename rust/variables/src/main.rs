b98391dea8f83b47f1e6bc294afc2ba8d0e3460e
Kevin Xi <kevin.xgr@gmail.com>
2017-04-30 17:03:31 +0800

    On types.

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -11,4 +11,19 @@ fn main() {
     println!("The value of x is: {}", x);
     let x = "haha"; // this is shadowing, just a new variable
     println!("The value of x is: {}", x);
+
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (a, _, b) = tup;
+    println!("The value of b is: {}", b);
+    // let c = tup.3;
+
+    // array: fixed length! on the stack!
+    // vector: can grow/shrink size
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    let fst = months[0];
+    println!("The value of fst is: {}", fst);
+    // a warning at build time, a panic at run time!
+    // let non = months[12];
 }
