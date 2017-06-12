8cacb08c5d292e131da81702f9805c0f39a5a498
Kevin Xi <kevin.xgr@gmail.com>
2017-06-12 23:48:22 +0800

    On io refactor: fn -> Config method.

--- rust/book/b12_greprs/src/main.rs
+++ rust/book/b12_greprs/src/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -14,22 +14,25 @@ fn main() {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("somthing went wrong reading the file");
+    f.read_to_string(&mut contents)
+        .expect("somthing went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
 struct Config {
     query: String,
-    filename: String
+    filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config {
-        query: query,
-        filename: filename
+        Config {
+            query: query,
+            filename: filename,
+        }
     }
 }
