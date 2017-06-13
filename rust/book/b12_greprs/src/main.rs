a34c2c992618d95cbdc718382e9a3b3daa3dc698
Kevin Xi <kevin.xgr@gmail.com>
2017-06-14 00:41:31 +0800

    On io refactor: abstract main logic and err handling to `run`.

--- rust/book/b12_greprs/src/main.rs
+++ rust/book/b12_greprs/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::process;
 use std::fs::File;
+use std::error::Error;
 use std::io::prelude::*;
 // use std::io::Read;   // will include this
 
@@ -15,13 +16,22 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {  // trait obj, a type that impl Error
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("somthing went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())  // idiomatic way to indicate `run` for side effect
 }
 
 struct Config {
