84e804c6d8f4d86b99a1e86ea6a307ce1060d16c
Kevin Xi <kevin.xgr@gmail.com>
2017-07-15 09:38:57 +0800

    fix write_bits: reorder bit numbering to Intel spec.

--- c/write_bits.c
+++ c/write_bits.c
@@ -1,15 +1,19 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 void write_bits(char* bits, char* to) {
     // no chk bit be '0' or '1'
-    int i = 0, len = (int)strlen(bits);
+    int i = 0;
+    size_t len = strlen(bits);
+    // take input as little-end
     int cur_val = 0, cur_pos = 0;
     for (; i < len; i++) {
-        // input simulate little-end
-        cur_val = ((bits[i] - '0') << (i%8)) + cur_val;
-        // input simulate big-end
-        // cur_val = (cur_val << 1) + (bits[i] - '0');
+    // if take input as big-end, should use
+    // int cur_val = 0, cur_pos = len / 8;
+    // and count the cur_pos back inside loop
+    // in order to get correct value
+        cur_val = (cur_val << 1) + (bits[i] - '0');
         if ((i+1) % 8 == 0) {
             to[cur_pos++] = cur_val;
             cur_val = 0;
