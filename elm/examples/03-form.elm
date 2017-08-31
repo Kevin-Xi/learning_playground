16427c21544aab15ee3c80d83df66e066f4ee164
Kevin Xi <kevin.xgr@gmail.com>
2017-09-01 01:31:02 +0800

    03: add feature verifies.

--- elm/examples/03-form.elm
+++ elm/examples/03-form.elm
@@ -1,7 +1,8 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
+import Html.Events exposing (onInput, onClick)
+import String
+import Char exposing (isUpper, isLower, isDigit)
 
 main =
   Html.beginnerProgram
@@ -19,12 +20,14 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age: String
+  , check: Bool
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" "" False
 
 
 
@@ -35,20 +38,27 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
+    | Check
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Name name ->
-      { model | name = name }
+      { model | name = name, check = False }
 
     Password password ->
-      { model | password = password }
+      { model | password = password, check = False }
 
     PasswordAgain password ->
-      { model | passwordAgain = password }
+      { model | passwordAgain = password, check = False }
 
+    Age age ->
+      { model | age = age, check = False }
+
+    Check ->
+      { model | check = True }
 
 
 -- VIEW
@@ -60,17 +70,29 @@ view model =
     [ input [ type_ "text", placeholder "Name", onInput Name ] []
     , input [ type_ "password", placeholder "Password", onInput Password ] []
     , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , input [ type_ "text", placeholder "Age", onInput Age ] []
+    , button [ onClick Check ] [ text "Submit" ]
     , viewValidation model
     ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+  if model.check then -- todo abstract this
+    let
+      (color, message) =
+        case String.toInt model.age of
+          Result.Err _ -> ("red", "age must be number")
+          _ ->
+          if String.length model.password <= 8 then
+            ("red", "Password must longer than 8 chars")
+          else if not (String.any isUpper model.password && String.any isLower model.password && String.any isDigit model.password) then
+            ("red", "Password must contains upper, lower, digit chars")
+          else if model.password == model.passwordAgain then
+            ("green", "OK")
+          else
+            ("red", "Passwords do not match!")
+    in
+      div [ style [("color", color)] ] [ text message ]
+  else
+    div [] []
