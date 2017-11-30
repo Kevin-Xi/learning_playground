bcd5886bd9a3f49e59c9c1b617f0341fa60f96a2
Kevin Xi <kevin.xgr@gmail.com>
2017-11-30 23:51:15 +0800

    node/generator: update push and impl pushAll.

--- node/generator/control_flow_pusher.js
+++ node/generator/control_flow_pusher.js
@@ -1,21 +1,37 @@
 'use strict';
 
-module.exports = function push(fn) {
-    let gen = fn();
-    let next = gen.next();
-    loop(next, gen);
+function push(genF, done) {
+    if (typeof done !== 'function') done = () => {};
+
+    const gen = genF();
+    recur(gen.next());  // todo: -> iter
+
+    function recur(cur) {
+        if (cur.done) return done();
+
+        if (cur.value instanceof Promise) {
+            cur.value.then(res => {
+                recur(gen.next(res));
+            }).catch(gen.throw.bind(gen));
+        } else {
+            recur(gen.next(cur.value));
+        }
+    }
 }
 
-function loop(next, gen) {
-    if (next.done) return;
-
-    if (next.value instanceof Promise) {
-        next.value.then(val => {
-            loop(gen.next(val), gen);
-        }).catch(err => {
-            gen.throw(err);
+function pushAll(genFs) {
+    const total = genFs.length;
+    let done = 0;
+    return new Promise((res, rej) => {
+        genFs.forEach(genF => {
+            push(genF, () => {
+                if (++done === total) res();
+            });
         });
-    } else {
-        loop(gen.next(next.value), gen);
-    }
+    });
+}
+
+module.exports = {
+    push,
+    pushAll
 }
